9672a9f9a5f18fca7113bd72a727f97d24b78b67
Maciej Adamski <m.adamski@ideacode.pl>
2018-11-05 14:17:52 +0100

    Base structure and styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,20 @@ var sassLintPlugin = require("gulp-sass-lint");
 var sourceMapPlugin = require("gulp-sourcemaps");
 var browserSync = require("browser-sync").create();
 
-gulp.task("default", gulp.series(lintSass, compileSass));
-gulp.task("watch-sync", gulp.series(lintSass, compileSass, function() {
+gulp.task("default", gulp.series(lintSass, compileSass, copyImages));
+gulp.task("watch-sync", gulp.series(lintSass, compileSass, copyImages, function() {
     browserSync.init({
         server: "./dist"
     });
 
     gulp.watch(
         "./source/scss/**/*.scss",
-        gulp.series(lintSass, compileSass)
+        gulp.series(lintSass, compileSass, copyImages)
+    );
+
+    gulp.watch(
+        "./source/images",
+        copyImages
     );
 }));
 
@@ -27,12 +32,19 @@ function lintSass(afterDone) {
 }
 
 function compileSass(afterDone) {
-    gulp.src("./source/scss/template.scss")
+    gulp.src(["./source/scss/template.scss", "./source/scss/demo.scss"])
         .pipe(sourceMapPlugin.init())
         .pipe(sassPlugin().on("error", sassPlugin.logError))
         .pipe(sourceMapPlugin.write("./"))
         .pipe(gulp.dest("./dist/css"))
         .pipe(browserSync.stream());
+
+    afterDone();
+}
+
+function copyImages(afterDone) {
+    gulp.src("./source/images/**/*")
+        .pipe(gulp.dest("./dist/images"));
 
     afterDone();
 }
